8f2e9d1e52b19992f42d685427171c5a07df0d53
hilinafissha <hilinafissha16@gmail.com>
2022-04-09 02:35:19 +0300

    pri

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -14,8 +14,7 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 if (alphabet == 'q')
 continue;
 
-else if (alphabet == 'e')
-continue;	
+else if (alphabet == 'e') continue;	
 
 putchar(alphabet);
 }
